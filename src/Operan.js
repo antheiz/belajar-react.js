406fb95d3ef8074ce9efdb3a8fd424be1651e76c
antroytheiz <theisandatu@gmail.com>
2021-02-20 09:55:02 +0900

    State dan props part 2

--- src/Operan.js
+++ src/Operan.js
@@ -2,20 +2,31 @@ import React, { Component } from 'react'
 
 export default class Operan extends Component {
 
-    // gantiMakanan(makananBaru) {
-    //     this.state({
-    //         makanan : makananBaru
+    // Tidak Bisa terganti dikarenakan dia Props
+    // gantiMakanan(makanan_baru) {
+    //     this.setState({
+    //         makanan:makanan_baru
     //     })
     // }
 
+    // Namun dapat diganti apabila di set dari file StatePropsnya (gantiMakanan) bukan dari operan
+    // Jadi kita harus menghapus function gantiMakanan di file Operan
+    // tapi kita harus oper function gantiMakanan dari StateProps ke file Operannya
+
     render() {
+        // Triks agar tidak mengulang tulis this.props
         const {makanan, gantiMakanan} = this.props
         return (
             <div>
-                <h2>Operan state yang menjadi props : {makanan}</h2>
-                <button onClick={() => gantiMakanan("Soto")}>Ganti Makanan</button>
+                <h2>Operan dari State ke Props : {makanan}</h2>
+                {/* Jika Props tidak ubah makan gunakan this.gantiMakanan */}
                 {/* <button onClick={() => this.gantiMakanan("Soto")}>Ganti Makanan</button> */}
+                {/* Jika hendak di ubah dari operan stateProps gunakan this.props.gantiMakanan */}
+                <button onClick={() => gantiMakanan("Soto")}>Ganti Makanan</button>
+
             </div>
         )
     }
+    // Jadi props adalah hasil operan komponen utama ke komponen bawaannya
+    // berupa state atau arrow function
 }
--- src/StateProps.js
+++ src/StateProps.js
@@ -1,29 +1,49 @@
 import React, { Component } from 'react'
-import Operan from './Operan';
+import Operan from './Operan'
 
 export default class StateProps extends Component {
+    // Kodingan yang pertama dieksekusi
     constructor(props) {
         super(props);
         this.state = {
-            makanan : "Bakso"
+            makanan: "Bakso"
         }
     }
 
-    gantiMakanan = (makananBaru) => {
-        this.setState({
-            makanan : makananBaru
+    // Jika error makan harus gunakan arrow function
+    // Lebih baik jika membuat fucntion gunakan Arrow Function
+    gantiMakanan = (makanan_baru) => {
+        this.setState ({
+            makanan:makanan_baru
         })
     }
 
     render() {
         return (
+            // Jsx atau htmlnya
             <div>
+                {/* STATE
+                State dapat diganti sedangkan Props tidak karena bersifat ReadOnly */}
+                {/* 
+                CONTOH STATE 1
+                <h2>{this.state.makanan}</h2>
+                <button onClick={() => this.setState({makanan:"Soto"}) }>Ganti Makanan</button> 
+                */}
+
+                {/* 
+                CONTOH STATE 2
+                Menggunakan Arrow Function
+                <h2>{this.state.makanan}</h2>
+                <button onClick={() => this.gantiMakanan("Soto")}>Ganti Makanan</button> 
+                */}
+
+                {/* PROPS 
+                Kapan state dapat di jadi Props, ketika di oper ke komponen lain
+                */}
+
                 <h2>{this.state.makanan}</h2>
                 <button onClick={() => this.gantiMakanan("Soto")}>Ganti Makanan</button>
                 <Operan makanan={this.state.makanan} gantiMakanan={this.gantiMakanan}/>
-                {/* <button onClick={() => this.setState({ makanan:"Soto" })}>Ganti Makanan</button> */}
-
-                {/* State dapat diubah - Props tak dapat diubah */}
             </div>
         )
     }
